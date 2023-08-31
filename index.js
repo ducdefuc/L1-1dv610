1c250f6b112ce96ba77af7b53f29cae6137d41bd
DucAnh Pham <ducanh.p@outlook.com>
2023-08-31 13:26:56 +0200

    added functionallity, start game question

--- index.js
+++ index.js
@@ -14,3 +14,30 @@ function greet(event) {
     nameForm.style.display = 'none'
     document.querySelector('#permissionQuestion').style.display = 'block'
 }
+
+document.querySelector('#permissionButton').addEventListener('click', askIfPlay)
+
+function askIfPlay(event) {
+    event.preventDefault()
+    
+    const userResponseElement = document.querySelector('input[name="guessPermission"]:checked')
+
+    if (userResponseElement) {
+        const userResponse = userResponseElement.value
+
+        if (userResponse === 'no') {
+            alert('Alright! Maybe next time!')
+            location.reload()
+        } else {
+            document.querySelector('#permissionQuestion').style.display = 'none'
+            nameForm.style.display = 'none'
+            const nameOutput = document.querySelector('#greetOutput')
+            nameOutput.style.display = 'none'
+            startGame()
+        }
+    }
+}
+
+function startGame() {
+    console.log('game started')
+}
